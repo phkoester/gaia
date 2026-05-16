fe24605438d510f04f4a8ad198c60937e9f3feff
Philip Köster <philip.koester@web.de>
2026-05-16 13:13:25 +0200

    ...

--- src/main/cmake/base.cmake
+++ src/main/cmake/base.cmake
@@ -216,7 +216,7 @@ AddVar(GAIA_BOOST_VERSION     STRING 1.90.0 "Boost version")     # https://githu
 AddVar(GAIA_FMT_VERSION       STRING 12.1.0 "{fmt} version")     # https://github.com/fmtlib/fmt
 AddVar(GAIA_GTEST_VERSION     STRING 1.17.0 "GTest version")     # https://github.com/google/googletest
 # XXX 78.2
-AddVar(GAIA_ICU_VERSION       STRING 76.1   "ICU version")       # sudo apt install libicu-dev
+AddVar(GAIA_ICU_VERSION       STRING 77.1   "ICU version")       # sudo apt install libicu-dev
 AddVar(GAIA_ROCKET_VERSION    STRING HEAD   "Rocket version")
 AddVar(GAIA_SCNLIB_VERSION    STRING master  "scnlib version")    # https://github.com/eliaskosunen/scnlib
 
