ff2d7294c0732e0ca62fc68ddb880eafcc14ab1f
Philip Köster <philip.koester@web.de>
2026-08-06 18:30:47 +0200

    (No commit message.)

--- src/main/make/Makefile-C.mk
+++ src/main/make/Makefile-C.mk
@@ -125,6 +125,9 @@ endif
 # Configure CTest -------------------------------------------------------------------------------------------
 
 CTEST_FLAGS := --output-on-failure
+ifdef GAIA_WINDOWS
+  CTEST_FLAGS += --test-dir build\src\test
+endif
 ifeq ($(VERBOSE),1)
   CTEST_FLAGS += -V
 endif
