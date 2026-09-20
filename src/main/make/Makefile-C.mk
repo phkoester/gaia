3de99c48aceb5596e59e07b67fa9b8d4d9a5548f
Philip Köster <philip.koester@web.de>
2026-09-20 15:36:39 +0200

    (No commit message.)

--- src/main/make/Makefile-C.mk
+++ src/main/make/Makefile-C.mk
@@ -239,8 +239,7 @@ ifneq ($(wildcard $(BUILD_DIR)/src/test/),)
 	@ctest $(CTEST_FLAGS) --preset $(TEST_PRESET) --test-dir $(BUILD_DIR)/src/test $(if $(PATTERN),-R '$(PATTERN)',)
   ifeq ($(COVERAGE),1)
 	@mkdir -p $(COVERAGE_DIR)
-	@lcov --capture --directory $(BUILD_DIR)/src \
-	  --ignore-errors inconsistent --output-file $(COVERAGE_DIR)/app.info
+	@lcov --capture --directory $(BUILD_DIR)/src --output-file $(COVERAGE_DIR)/app.info
 	@genhtml --ignore-errors inconsistent \
 	  -o $(COVERAGE_DIR)/html $(COVERAGE_DIR)/app.info
 	@# find -name "*.gcda" -type f -delete
