e0697fa388fa67ba0aeb4ca18678a43559017b75
Philip Köster <philip.koester@web.de>
2026-09-20 15:40:30 +0200

    (No commit message.)

--- src/main/make/Makefile-C.mk
+++ src/main/make/Makefile-C.mk
@@ -238,13 +238,16 @@ ifneq ($(wildcard $(BUILD_DIR)/src/test/),)
 	@$(call print-target,$@)
 	@ctest $(CTEST_FLAGS) --preset $(TEST_PRESET) --test-dir $(BUILD_DIR)/src/test $(if $(PATTERN),-R '$(PATTERN)',)
   ifeq ($(COVERAGE),1)
+    ifndef GAIA_WINDOWS
 	@mkdir -p $(COVERAGE_DIR)
-	@lcov --capture --directory $(BUILD_DIR)/src --output-file $(COVERAGE_DIR)/app.info
-	@genhtml --ignore-errors inconsistent \
-	  -o $(COVERAGE_DIR)/html $(COVERAGE_DIR)/app.info
-	@# find -name "*.gcda" -type f -delete
+	@lcov --capture --directory $(BUILD_DIR)/src \
+	  --ignore-errors inconsistent --output-file $(COVERAGE_DIR)/app.info
+	@genhtml --ignore-errors inconsistent -o $(COVERAGE_DIR)/html $(COVERAGE_DIR)/app.info
 	@rm $(COVERAGE_DIR)/app.info
 	@echo Created $(COVERAGE_DIR)/html/index.html
+    else
+      $(error lcov is not supported on Windows)
+    endif
   endif
 endif
 
